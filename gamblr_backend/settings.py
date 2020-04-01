45f5ef614752da518457d7470f8c661709b1edae
Kyle Xiao <kylepxiao@gmail.com>
2020-04-01 03:01:36 -0400

    settings

--- gamblr_backend/settings.py
+++ gamblr_backend/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'rest_framework',
 ]
 
@@ -127,7 +126,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-
-SITE_ID=1
 
 django_heroku.settings(locals())
